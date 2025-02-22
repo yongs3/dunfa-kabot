e61119446efa6220c063e8a255bacfb3f917940f
yongs <artemi13221@gmail.com>
2025-02-22 15:34:15 +0000

    format_datetime function update

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
 import requests
+from datetime import datetime, timedelta
 
 load_dotenv()
 
 app = Flask(__name__)
 
 dunfa_api_key = os.getenv('DUNFA_API_KEY')
-dunfa_basic_url = https://api.neople.co.kr/df/
+dunfa_basic_url = 'https://api.neople.co.kr/df/'
+
+def format_datetime(dt):
+    return dt.strftime('%Y%m%dT%H%M')
 
 def get_dunfa_timeline(character_info, date_info, limit_info=100, next_info=None):
-    dunfa_timeline_url = dunfa_basic_url + 'servers/' + character_info.serverid + '/characters/' + character_info.characterid + '/timeline'
+    dunfa_timeline_url = dunfa_basic_url + 'servers/' + character_info['serverid'] + '/characters/' + character_info['characterid'] + '/timeline'
 
     params = {
         'limit': limit_info,
-        'startDate': date_info.start_date, 
-        'endDate': date_info.end_date
+        'startDate': date_info['start_date'], 
+        'endDate': date_info['end_date']
     }
 
     if next_info is not None:
@@ -44,4 +48,6 @@ def get_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    print(get_yesterday_date_info())
+    print(format_datetime(datetime.now()))
