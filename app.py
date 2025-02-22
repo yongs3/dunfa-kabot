b40779fec4cebc27bfd08f9c577a0eda66407f31
yongs <artemi13221@gmail.com>
2025-02-22 06:58:43 +0000

    timeline function update

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
 
 app = Flask(__name__)
+
+dunfa_api_key = os.getenv('DUNFA_API_KEY')
+dunfa_basic_url = https://api.neople.co.kr/df/
+
+def get_dunfa_timeline(character_info, date_info, limit_info=100, next_info=None):
+    dunfa_timeline_url = dunfa_basic_url + 'servers/' + character_info.serverid + '/characters/' + character_info.characterid + '/timeline'
+
+    params = {
+        'limit': limit_info,
+        'startDate': date_info.start_date, 
+        'endDate': date_info.end_date
+    }
+
+    if next_info is not None:
+        params['next'] = next_info
+    
+    params['apikey'] = dunfa_api_key
+
+    response = requests.get(dunfa_timeline_url, params=params)
+
+    return response.json()
 
 @app.route('/', methods=['GET'])
 def get_data():
