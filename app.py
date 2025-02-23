f892065a59550e22cc573967aef7054f62f7d801
yongs <artemi13221@gmail.com>
2025-02-23 09:44:49 +0000

    count item function update

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ dunfa_basic_url = 'https://api.neople.co.kr/df/'
 def format_datetime(dt):
     return dt.strftime('%Y%m%dT%H%M')
 
+def get_today_date_info():
+    result = datetime.now().replace(hour=6, minute=0)
+    return format_datetime(result)
+
+def get_yesterday_date_info():
+    result = (datetime.now() - timedelta(days=1)).replace(hour=6, minute=0)
+    return format_datetime(result)
+
 def get_dunfa_timeline(character_info, date_info, limit_info=100, next_info=None):
     dunfa_timeline_url = dunfa_basic_url + 'servers/' + character_info['serverid'] + '/characters/' + character_info['characterid'] + '/timeline'
 
@@ -32,6 +40,31 @@ def get_dunfa_timeline(character_info, date_info, limit_info=100, next_info=None
 
     return response.json()
 
+def count_item_rarity(timeline_data):
+    legendary_count = 0
+    epic_count = 0
+    taecho_count = 0
+
+    for i in timeline_data['timeline']['rows']:
+        if i['data']['itemRarity'] == '레전더리':
+            legendary_count = legendary_count + 1
+        elif i['data']['itemRarity'] == '에픽':
+            epic_count = epic_count + 1
+        elif i['data']['itemRarity'] == '태초':
+            taecho_count = taecho_count + 1
+    
+    return (taecho_count, epic_count, legendary_count)
+
+
+def get_dunfa_yesterday_total_items(character_info):
+    date_info = {
+        'start_date': get_yesterday_date_info(),
+        'end_date': get_today_date_info()
+    }
+    timeline_result = get_dunfa_timeline(character_info, date_info)
+
+    return count_item_rarity(timeline_result)
+
 @app.route('/', methods=['GET'])
 def get_data():
     data = {
@@ -49,5 +82,3 @@ def get_data():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    print(get_yesterday_date_info())
-    print(format_datetime(datetime.now()))
